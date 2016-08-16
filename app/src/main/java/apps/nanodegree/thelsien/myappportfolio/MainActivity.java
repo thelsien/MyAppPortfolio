60b0219ec17dcd4103137ccd1c396680b8538cb6
thelsien <789iuz@gmail.com>
2016-08-16 18:50:37 +0200

    I made changes according to the result of the review.

--- app/src/main/java/apps/nanodegree/thelsien/myappportfolio/MainActivity.java
+++ app/src/main/java/apps/nanodegree/thelsien/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package apps.nanodegree.thelsien.myappportfolio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,27 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onPopularMoviesClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Popular Movies\" app!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onStockHawkClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Stock Hawk\" app!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onBuildItBiggerClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Build It Bigger\" app!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onMakeYourAppMaterialClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Make Your App Material\" app!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onGoUbiquitousClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Go Ubiquitous\" app!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onCapstoneClicked(View clickedView) {
-        Toast.makeText(this, "This button will launch my \"Capstone\" app!", Toast.LENGTH_SHORT).show();
+    public void onAppButtonClicked(View clickedView) {
+        String btnText = ((Button) clickedView).getText().toString();
+        Toast.makeText(this, "This button will launch my \"" + btnText + "\" app!", Toast.LENGTH_SHORT).show();
     }
 }
